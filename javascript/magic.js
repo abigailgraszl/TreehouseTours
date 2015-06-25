5287c2796d559bc4d2a6ed74d0664b9515797527
Abigail Graszl <acgraszl@gmail.com>
2015-06-24 22:48:17 -0400

    completed treehouses page with jquery

--- javascript/magic.js
+++ javascript/magic.js
@@ -1,13 +1,51 @@
 $(document).ready(function(){
 
-	$("#photo1").mouseenter(function(){
-		$("#cover1").slideUp();
-
+	$("#tree1").hover(function(){
+		$("#box1").fadeIn("fast");
+	}, function(){
+		$("#box1").fadeOut("fast");
 	});
 
+	$("#tree2").hover(function(){
+		$("#box2").fadeIn("fast");
+	}, function(){
+		$("#box2").fadeOut("fast");
+	});
 
+	$("#tree3").hover(function(){
+		$("#box3").fadeIn("fast");
+	}, function(){
+		$("#box3").fadeOut("fast");
+	});
 
+	$("#tree4").hover(function(){
+		$("#box4").fadeIn("fast");
+	}, function(){
+		$("#box4").fadeOut("fast");
+	});
 
+	$("#tree5").hover(function(){
+		$("#box5").fadeIn("fast");
+	}, function(){
+		$("#box5").fadeOut("fast");
+	});
 
+	$("#tree6").hover(function(){
+		$("#box6").fadeIn("fast");
+	}, function(){
+		$("#box6").fadeOut("fast");
+	});
+
+	$("#tree7").hover(function(){
+		$("#box7").fadeIn("fast");
+	}, function(){
+		$("#box7").fadeOut("fast");
+	});
+
+	$("#tree8").hover(function(){
+		$("#box8").fadeIn("fast");
+	}, function(){
+		$("#box8").fadeOut("fast");
+	});
 
 });
